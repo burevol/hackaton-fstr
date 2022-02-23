7aa24720a3cffd127b697be1975b69bc8d227c8f
burevol <alexum2013@yandex.ru>
2022-02-23 18:38:16 +0300

    add module utils

---
+++ utils/validators.py
@@ -0,0 +1,43 @@
+from typing import Optional, List
+
+from pydantic import BaseModel
+
+
+class Coord(BaseModel):
+    latitude: str
+    longitude: str
+    height: str
+
+
+class Level(BaseModel):
+    winter: str
+    summer: str
+    autumn: str
+    spring: str
+
+
+class Image(BaseModel):
+    url: str
+    title: str
+
+
+class User(BaseModel):
+    id: Optional[str]
+    email: Optional[str]
+    phone: Optional[str]
+    fam: Optional[str]
+    name: Optional[str]
+    otc: Optional[str]
+
+
+class Item(BaseModel):
+    beautyTitle: str
+    title: str
+    other_titles: str
+    connect: str
+    add_time: Optional[str]
+    coords: Coord
+    type: str
+    level: Level
+    user: User
+    images: List[Image] = []
