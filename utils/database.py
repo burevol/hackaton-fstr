2512b173836361490286043b16f97a96002ed7aa
burevol <alexum2013@yandex.ru>
2022-02-23 19:53:46 +0300

    add timestamp

--- utils/database.py
+++ utils/database.py
@@ -1,5 +1,7 @@
-import psycopg2
 import json
+from datetime import datetime
+
+import psycopg2
 
 import utils.config as config
 
@@ -9,7 +11,10 @@ def send_data(raw_data):
     conn = psycopg2.connect(dbname=config.FSTR_DB_NAME, user=config.FSTR_DB_LOGIN,
                             password=config.FSTR_DB_PASS, host=config.FSTR_DB_HOST, port=config.FSTR_DB_PORT)
     cur = conn.cursor()
-    cur.execute('INSERT INTO "public"."pereval_added" ("raw_data", "images") VALUES (%s, %s) RETURNING id', (json.dumps(raw_data), images))
+    dt = datetime.now()
+    cur.execute(
+        'INSERT INTO "public"."pereval_added" ("raw_data", "date_added", "images") VALUES (%s, %s, %s) RETURNING id',
+        (json.dumps(raw_data), dt, images))
     last_row_id = cur.fetchone()[0]
     conn.commit()
     cur.close()
