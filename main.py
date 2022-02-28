31b229f2963785ba6dffc3fa048df9caad2a19e8
burevol <alexum2013@yandex.ru>
2022-02-28 20:03:36 +0300

    добавлено логирование

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import psycopg2
 import requests
 import uvicorn
+import logging
 from fastapi import FastAPI, Request, status
 from fastapi.encoders import jsonable_encoder
 from fastapi.exceptions import RequestValidationError
@@ -34,6 +35,7 @@ async def submit_data(item: Item):
 
 @app.exception_handler(RequestValidationError)
 async def validation_exception_handler(request: Request, exc: RequestValidationError):
+    logging.info(exc.args[0])
     return JSONResponse(
         status_code=status.HTTP_400_BAD_REQUEST,
         content=jsonable_encoder({'status': 400,
